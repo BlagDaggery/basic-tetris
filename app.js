4f0ff288537dd412698e3a448580f49b78ac0387
Blaine Anderson <banderson@silverchair.com>
2020-05-23 13:56:26 -0400

    Create variables for page elements

---
+++ app.js
@@ -0,0 +1,5 @@
+const Grid = document.querySelector('.grid');
+let squares = Array.from(document.querySelectorAll('.grid div'));
+const Width = 10;
+const ScoreDisplay = document.querySelector('#score');
+const StartButton = document.querySelector('#start-button');
